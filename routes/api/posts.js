9e43af5a3b8fe0ebec23ce44d7512e37ddb780ca
Philip Tenteromano <tenteromano.p@gmail.com>
2018-12-12 01:37:23 -0500

    init server and routes

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+// middleware for routes
+const express = require("express");
+const router = express.Router();
+
+// @route GET /api/posts/test
+// @desc Tests post route
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+// middleware for routes
+const express = require("express");
+const router = express.Router();
+
+// @route GET /api/users/test
+// @desc Tests users route
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "User works" }));
+
+module.exports = router;
